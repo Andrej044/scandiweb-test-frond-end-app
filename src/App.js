b025653f969f8c226c60d9e80e216a2109bafffd
Andrii Lishchyna <andrej044@i.ua>
2022-09-20 17:28:09 +0200

    Update App.js
    Changed basic  HTML structure on two React component Header & Main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import Header from "./layouts/Header";
+import Main from "./layouts/Main";
+
 
 export default class App extends Component{
   constructor(props){
@@ -9,32 +12,8 @@ export default class App extends Component{
   render(){
     return(
       <>
-        <header>
-          <nav>
-            <ul>
-              <li><a href="#">All</a></li>
-              <li><a href="#">Tecch</a></li>
-              <li><a href="#">Clothes</a></li>
-            </ul>
-            <ul>
-              <li>Currency</li>
-              <li>Basket</li>
-            </ul>
-          </nav>
-        </header>
-        <main>
-          <h1>Category name</h1>
-          <section>
-              <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
-                <li>Product 4</li>
-                <li>Product 5</li>
-                <li>Product 6</li>
-              </ul>
-          </section>
-        </main>
+        <Header/>
+        <Main/>
       </>
     )
   }
