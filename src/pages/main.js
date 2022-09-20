05e7c03e5dcb3765c32f3e1dcef5ea413ebb4948
Andrii Lishchyna <andrej044@i.ua>
2022-09-20 16:46:26 +0200

    Init main page

---
+++ src/pages/main.js
@@ -0,0 +1,22 @@
+import React,{Component} from "react";
+
+export default class Main extends Component{
+    render(){
+        return(
+                <main>
+                    <h1>Category name</h1>
+                    <section>
+                        <h2>Main Component</h2>
+                        <ul>
+                            <li>Product 1</li>
+                            <li>Product 2</li>
+                            <li>Product 3</li>
+                            <li>Product 4</li>
+                            <li>Product 5</li>
+                            <li>Product 6</li>
+                        </ul>
+                    </section>
+                </main>
+        )
+    }
+}
