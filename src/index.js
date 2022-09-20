9885133c942153b3413c27a6e44ca10d4911d858
Andrii Lishchyna <andrej044@i.ua>
2022-09-20 18:58:24 +0200

    Initialize ApolloClient

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
 import './styles/index.css';
 import App from './App';
+
+
+const  client = new ApolloClient({
+    uri:"http://localhost:4000/",
+    cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
