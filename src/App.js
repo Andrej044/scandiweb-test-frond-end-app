b52a62a469ad50b2f70c7303389603fc133c7fd2
Andrii Lishchyna <andrej044@i.ua>
2022-09-20 16:10:20 +0200

    Add basic html structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-         <p>
-          Editand save to reload.
-        </p>
-
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={}
+  }
+  
+  render(){
+    return(
+      <>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="#">All</a></li>
+              <li><a href="#">Tecch</a></li>
+              <li><a href="#">Clothes</a></li>
+            </ul>
+            <ul>
+              <li>Currency</li>
+              <li>Basket</li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <h1>Category name</h1>
+          <section>
+              <ul>
+                <li>Product 1</li>
+                <li>Product 2</li>
+                <li>Product 3</li>
+                <li>Product 4</li>
+                <li>Product 5</li>
+                <li>Product 6</li>
+              </ul>
+          </section>
+        </main>
+      </>
+    )
+  }
 }
-
-export default App;
