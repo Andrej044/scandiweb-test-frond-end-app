ff95c23075843b614a34b62eeac0b08c82d4b985
Andrii Lishchyna <andrej044@i.ua>
2022-09-20 17:28:42 +0200

    Create Main.js
    Init Main layot component

---
+++ src/layouts/Main.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import HomePage from "../pages/HomePage";
+
+export default class Main extends Component{
+    render(){
+        return(
+                <main>
+                    <HomePage/>
+                </main>
+        )
+    }
+}
