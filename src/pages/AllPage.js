f7bca9b3248cb1a1bf0ee80a7f08cb5ce9c967ab
Andrii Lishchyna <andrej044@i.ua>
2022-09-24 11:27:59 +0200

    Create AllPage.js

---
+++ src/pages/AllPage.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+import Products from "../layouts/Products";
+
+export default class AllPage extends Component{
+    render(){
+        const categoryAll = this.props.dataCategories.data.filter(item => {
+            return item.name === "all"
+        })
+        return(
+                <>
+                    <Products categoryName= {categoryAll}/>
+                </>
+        )
+    }
+}
