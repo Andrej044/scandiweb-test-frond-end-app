65f2afd0d313d7b2cde412e6678639cae50dc72a
Andrii Lishchyna <andrej044@i.ua>
2022-09-24 13:10:54 +0200

    Create CurrencySwitcher.js

---
+++ src/components/CurrencySwitcher.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+
+
+export default class CurrencySwitcher extends Component {
+
+
+    render(){
+        return(
+            CurrencySwitcher
+        )
+    }
+}
