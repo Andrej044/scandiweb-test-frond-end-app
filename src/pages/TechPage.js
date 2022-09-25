39ea1230f1d5ef01d8fe35df5139a3877d682b57
Andrii Lishchyna <andrej044@i.ua>
2022-09-25 13:27:55 +0200

    Create TechPage.js

---
+++ src/pages/TechPage.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+import Products from "../layouts/Products";
+
+export default class AllPage extends Component{
+    render(){
+        const categoryAll = this.props.dataCategories.data.filter(item => {
+            return item.name === "tech"
+        })
+        return(
+            <>
+                <Products categoryName= {categoryAll} currency = {this.props.dataCategories}/>
+            </>
+        )
+    }
+}
