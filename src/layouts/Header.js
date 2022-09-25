085747ab5c5f6f85cbc0fc4c8ee391018142d6e6
Andrii Lishchyna <andrej044@i.ua>
2022-09-25 13:27:39 +0200

    Update Header.js
    Implement routing

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,20 +1,35 @@
 import React,{Component} from "react";
+import {NavLink} from "react-router-dom";
+import logo from "../images/logo.svg"
+import CurrencySwitcher from "../components/CurrencySwitcher";
 
 
 export default class Header extends Component{
     render(){
+        const {currencyChanger} = this.props
+        const categoryList =  this.props.data.dataCategories.map(item => (
+            <li key={item.name}>
+               <NavLink to={`/${item.name}`} > { item.name}</NavLink>
+            </li>
+        ));
+        const currencyList =  this.props.data.dataCurrencies.map(item => ({
+            currencyLabel: item.label,
+            currencySymbol: item.symbol
+            })
+        )
         return(
         <header>
             <nav>
                 <ul>
-                    <li><a href="#">All</a></li>
-                    <li><a href="#">Tecch</a></li>
-                    <li><a href="#">Clothes</a></li>
+                    {categoryList}
                 </ul>
-                <ul>
-                    <li>Currency</li>
-                    <li>Basket</li>
-                </ul>
+                <span>
+                    <img src={logo} width="31" height="30" alt="logo" title="logo"/>
+                </span>
+                 <div className="currencySwitcher">
+                     <CurrencySwitcher currencies = {currencyList} currencyChanger = {currencyChanger} state = {this.props.data} />
+                 </div>
+
             </nav>
         </header>
         )
