32f939dfb12cdeb92dbfeaab990f6b6aa1729c04
Andrii Lishchyna <andrej044@i.ua>
2022-09-25 13:26:50 +0200

    Update CurrencySwitcher.js
    Implemented  reverse data flow

--- src/components/CurrencySwitcher.js
+++ src/components/CurrencySwitcher.js
@@ -1,13 +1,46 @@
-import React,{Component} from "react";
-
-
+import React, {Component, createElement} from "react";
 
 export default class CurrencySwitcher extends Component {
-
+    state = {
+        isShow: false
+    }
+    handleShowOn = () => {
+        this.setState({
+            isShow:true
+        })
+    }
+    handleShowOff = () => {
+        this.setState({
+            isShow:false
+        })
+    }
+    handleOff = () => {
+        this.setState({isShow:false})
+    }
 
     render(){
+        const currencyChanger = this.props.currencyChanger;
+        const {symbol:currencySymbol} = this.props.state.currencies;
+
+        const currenciesList = this.props.currencies.map(currency => (
+            <li
+                className="currency-item"
+                onClick={() => {
+                currencyChanger(currency.currencyLabel,currency.currencySymbol );
+                this.handleShowOff();
+                }}
+                key= {currency.currencyLabel}
+                value={currency.currencyLabel}
+            >
+                {currency.currencySymbol} {currency.currencyLabel}
+            </li>
+        ));
+
         return(
-            CurrencySwitcher
+            <>
+                <span onClick= {this.handleShowOn}>{currencySymbol}</span>
+                {this.state.isShow ? <ul className="currencySwitcher">{currenciesList}</ul> : null }
+            </>
         )
     }
 }
