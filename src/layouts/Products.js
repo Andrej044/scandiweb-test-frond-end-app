ea3fab89adac37613c8a572b253ef2956d9c9867
Andrii Lishchyna <andrej044@i.ua>
2022-09-29 19:07:47 +0200

    Update Products.js

--- src/layouts/Products.js
+++ src/layouts/Products.js
@@ -1,33 +1,32 @@
 import React,{Component} from "react";
+import {NavLink} from "react-router-dom";
+import Product from "../components/Product";
 
 
 export default class Products extends Component{
 
     findCurrency = (arr = [], findElement) => {
-      let findedCurrency =  arr.filter(item=>{
+     return(arr.filter(item => {
           if(item.currency.label === findElement) return item
-        })
-        return findedCurrency
+        }))
     }
     render() {
-            const {currency} = this.props.currency
-            let products = this.props.categoryName[0] === undefined ?  [] : this.props.categoryName[0].products
+            const {currency} = this.props.currency;
+            let products = this.props.categoryName[0] === undefined ?  [] : this.props.categoryName[0].products;
             const productList = products.map(product => {
-              const currentCurrency =  this.findCurrency(product.prices, currency.label);
-                  return (
-                      <div key= { product.id} className="card">
-                        <img src= {product.gallery[0]} alt=""/>
-                        <p>{product.name}</p>
-                        <p>{currentCurrency[0].amount}<span>{currentCurrency[0].currency.symbol}</span></p>
-                    </div>)
+                const currentCurrency =  this.findCurrency(product.prices, currency.label);
+                return (
+                    <NavLink to={`/product/${product.id}`} key= { product.id}>
+                        <Product currency = {currentCurrency} product={product}/>
+                    </NavLink>
+                )
     })
 
 
         return(
             <>
-                <h1>Category name</h1>
                 <section>
-                    <h2>Main Component</h2>
+                    <h2>{this.props.categoryName[0] === undefined ? "Category name" : this.props.categoryName[0].name }</h2>
                     <div className="card-wrapper">
                         <div className="card-category">
                             {productList}
