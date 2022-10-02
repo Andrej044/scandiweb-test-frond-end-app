80c70e40a61a6104ec32a9ee253badab9c749f1d
Andrii Lishchyna <andrej044@i.ua>
2022-10-02 14:34:56 +0200

    Create Attributes.js
    Create attributes component

---
+++ src/layouts/Attributes.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+import Form from "../components/Form";
+import ColorPicker from "../components/ColorPicker";
+import SizePicker from "../components/SizePicker";
+
+export default class Attributes extends Component{
+    render() {
+        const {attributes} = this.props;
+        // console.log(attributes)
+        const attributesList = attributes.map( attr => (
+            <div key={attr.id}>
+                 <h3 >{attr.id}:</h3>
+                <Form attribute = {attr} />
+            </div>
+        ))
+
+        return(
+            attributes.length > 0 ? <div>{attributesList}</div> : null
+        )
+    }
+}
