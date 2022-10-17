bc0ad6dbe389da7cc9d225f5a69a34961e305ec0
Andrii Lishchyna <andrej044@i.ua>
2022-10-17 16:23:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql} from '@apollo/client';
 
 import './styles/index.css';
 import App from './App';
@@ -11,13 +11,14 @@ const  client = new ApolloClient({
     cache: new InMemoryCache()
 })
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <>
       <ApolloProvider client={client}>
-        <App client={client} />
+        <App/>
       </ApolloProvider>
-  </React.StrictMode>
+    </>
 );
 
 
