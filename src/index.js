3444fdcd659cd94f7c53a735e0a03227e9fb38c1
Andrii Lishchyna <andrej044@i.ua>
2022-10-18 17:45:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql} from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 import './styles/index.css';
 import App from './App';
