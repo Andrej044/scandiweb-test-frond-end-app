af7b0abdd6eb5fd9a4be629301831ccbb32eb980
Andrii Lishchyna <andrej044@i.ua>
2022-11-05 14:44:38 +0100

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,13 +1,21 @@
 import React, {Component} from "react";
 
-
+import CartComponent from "../components/CartComponent";
 
 export default class CartPage extends Component{
 
-
     render(){
+        const isCartPage = "true";
         return(
-            <div>Hello page</div>
+            <>
+                <h2>Cart</h2>
+                <CartComponent
+                    cart = {this.props.cart}
+                    duplicateProduct = {this.props.duplicateProduct}
+                    removeDuplicateProduct = {this.props.removeDuplicateProduct}
+                    isCartPage = {isCartPage}
+                />
+            </>
         )
     }
 }
